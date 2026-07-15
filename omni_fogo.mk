ac4f7d3ea1f487ff62305166767b40671ea531a4
cfeedback <50532171+cfeedback@users.noreply.github.com>
2026-07-14 22:28:15 -0700

    Add omni_fogo.mk for Motorola Fogo device

---
+++ omni_fogo.mk
@@ -0,0 +1,7 @@
+PRODUCT_NAME := omni_fogo
+PRODUCT_DEVICE := fogo
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto fogo
+PRODUCT_MANUFACTURER := motorola
+
+$(call inherit-product, device/motorola/fogo/device.mk)
